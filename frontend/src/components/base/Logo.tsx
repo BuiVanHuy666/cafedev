1951a8299e71d39e7a5df3f7a38670a149b601d6
Bui Van Huy <huy423210063@caodangvietmy.edu.vn>
2026-07-20 19:20:49 +0700

    init frontend with shadcn, tailwindcss, next-intl and font

---
+++ frontend/src/components/base/Logo.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export function Logo({className = "w-10 h-10"}: {
+	className?: string
+}) {
+	return (
+			<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 200 200"
+					className={className}
+			>
+				<g
+						stroke="currentColor"
+						strokeWidth="12"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						fill="none"
+				>
+					<path d="M 85 52 C 75 35 105 20 85 5"/>
+					<path d="M 115 52 C 105 35 135 20 115 5"/>
+					<ellipse cx="100" cy="75" rx="65" ry="16"/>
+					<path d="M 35 75 L 45 125 C 55 165 145 165 155 125 L 165 75"/>
+					<path d="M 162 95 C 195 95 195 135 153 135"/>
+					<path d="M 45 170 C 80 185 120 185 155 170"/>
+					<path d="M 75 102 L 55 117 L 75 132"/>
+					<path d="M 105 95 L 95 140"/>
+					<path d="M 125 102 L 145 117 L 125 132"/>
+				</g>
+			</svg>
+	);
+}
